3f1bdb348b21e6c253629f181302e0521d50e296
Ironhammer Std <2482911962@qq.com>
2025-12-01 15:48:02 +0800

    Revert "Datach debugger at exit"
    This reverts commit 0aef766058aad860ff95f193719ae74a8376ddf0.

--- Common/LocalLibManager.cpp
+++ Common/LocalLibManager.cpp
@@ -469,18 +469,8 @@ namespace Local
 
 	void __cdecl ExitClearImpl()
 	{
-		//datach from debugger
-		HANDLE hProcess = GetCurrentProcess();
-		if (IsDebuggerPresent())
-			DebugActiveProcessStop(GetProcessId(hProcess));
-		
-		//Terminate all remote components
 		RemoteComponentManager::Uninitialize();
-		
-		//Clear framework data
 		ExitClear();
-
-		//Close debug console
 		ECDebug::CloseDebugConsole();
 	}
 
