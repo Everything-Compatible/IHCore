eb15fb31454f1a277223d797f8a9d1d3f5b99cab
Ironhammer Std <2482911962@qq.com>
2025-12-19 01:34:47 +0800

    Remove Unused Files II

--- IHCore/MiscFunc.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <YRPP.h>
-
-bool __declspec(dllexport) PlayAnimOnObject(AnimTypeClass* pAnimType, ObjectClass* Object, byte Iteration);//0xFFu = Infinite Loop
-bool __declspec(dllexport) PlayAnimOnObject(const char* AnimID, ObjectClass* Object, byte Iteration);//0xFFu = Infinite Loop
